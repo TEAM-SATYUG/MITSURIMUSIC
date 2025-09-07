2717e9f07e2eb45a1b6ac85578ad5e0ac22e75f8
Team-Mitsuri <mitsurit42@gmail.com>
2025-09-07 13:55:58 +0530

    Update channelplay.py

--- MITSURIMUSIC/utils/channelplay.py
+++ MITSURIMUSIC/utils/channelplay.py
@@ -1,5 +1,5 @@
-from ANNIEMUSIC import app
-from ANNIEMUSIC.utils.database import get_cmode
+from MITSURIMUSIC import app
+from MITSURIMUSIC.utils.database import get_cmode
 
 
 async def get_channeplayCB(_, command, CallbackQuery):
