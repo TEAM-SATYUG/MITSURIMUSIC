f449242e15058b4ca0197d5a6420cffe3b5cf758
Team-Mitsuri <mitsurit42@gmail.com>
2025-09-07 19:31:52 +0530

    Update stop.py

--- MITSURIMUSIC/plugins/admins/stop.py
+++ MITSURIMUSIC/plugins/admins/stop.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from ANNIEMUSIC import app
-from ANNIEMUSIC.core.call import JARVIS
-from ANNIEMUSIC.utils.database import set_loop
-from ANNIEMUSIC.utils.decorators import AdminRightsCheck
-from ANNIEMUSIC.utils.inline import close_markup
+from MITSURIMUSIC import app
+from MITSURIMUSIC.core.call import TOXIC
+from MITSURIMUSIC.utils.database import set_loop
+from MITSURIMUSIC.utils.decorators import AdminRightsCheck
+from MITSURIMUSIC.utils.inline import close_markup
 from config import BANNED_USERS
 
 
@@ -16,7 +16,7 @@ from config import BANNED_USERS
 async def stop_music(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return
-    await JARVIS.stop_stream(chat_id)
+    await TOXIC.stop_stream(chat_id)
     await set_loop(chat_id, 0)
     await message.reply_text(
         _["admin_5"].format(message.from_user.mention), reply_markup=close_markup(_)
