c9021313245398f8ece4cf4439a16b4a46fbbe43
Team-Mitsuri <mitsurit42@gmail.com>
2025-09-09 15:45:06 +0530

    Update truth_dare.py

--- MITSURIMUSIC/plugins/misc/truth_dare.py
+++ MITSURIMUSIC/plugins/misc/truth_dare.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 from pyrogram.enums import ParseMode
 import httpx
-from ANNIEMUSIC import app
+from MITSURIMUSIC import app
 
 TRUTH_API = "https://api.truthordarebot.xyz/v1/truth"
 DARE_API = "https://api.truthordarebot.xyz/v1/dare"
