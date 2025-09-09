61c5500957ee91e14a93b096fa285b17e244901e
Team-Mitsuri <mitsurit42@gmail.com>
2025-09-09 16:16:20 +0530

    Update post.py

--- MITSURIMUSIC/plugins/sudo/post.py
+++ MITSURIMUSIC/plugins/sudo/post.py
@@ -1,8 +1,8 @@
 from pyrogram import filters
-from ANNIEMUSIC import app
+from MITSURIMUSIC import app
 from config import OWNER_ID
 
-destination_group_id = -1002076109983
+destination_group_id = 
 
 @app.on_message(filters.command(["post"], prefixes=["/", "."]) & filters.user(OWNER_ID))
 async def copy_messages(_, message):
