bd4d8494e11b66ae348db694b0cb68a67860eab2
Team-Mitsuri <mitsurit42@gmail.com>
2025-09-10 17:37:18 +0530

    Update speedtest.py

--- MITSURIMUSIC/plugins/tools/speedtest.py
+++ MITSURIMUSIC/plugins/tools/speedtest.py
@@ -4,9 +4,9 @@ import speedtest
 from pyrogram import filters
 from pyrogram.types import Message
 
-from ANNIEMUSIC import app
-from ANNIEMUSIC.misc import SUDOERS
-from ANNIEMUSIC.utils.decorators.language import language
+from MITSURIMUSIC import app
+from MITSURIMUSIC.misc import SUDOERS
+from MITSURIMUSIC.utils.decorators.language import language
 
 
 def run_speedtest():
