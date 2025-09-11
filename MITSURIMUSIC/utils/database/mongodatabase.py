b39f0b0fc8f9360392ad04d6dd29e598288485f7
Team-Mitsuri <mitsurit42@gmail.com>
2025-09-11 16:34:19 +0530

    Update mongodatabase.py

--- MITSURIMUSIC/utils/database/mongodatabase.py
+++ MITSURIMUSIC/utils/database/mongodatabase.py
@@ -499,10 +499,10 @@ async def remove_sudo(user_id: int) -> bool:
     return True
     
 
-# Gban Users
+# Gmute Users
 
 
-async def get_banned_users() -> list:
+async def get_muted_users() -> list:
     results = []
     async for user in blockeddb.find({"user_id": {"$gt": 0}}):
         user_id = user["user_id"]
@@ -510,29 +510,29 @@ async def get_banned_users() -> list:
     return results
 
 
-async def get_banned_count() -> int:
+async def get_muted_count() -> int:
     users = blockeddb.find({"user_id": {"$gt": 0}})
     users = await users.to_list(length=100000)
     return len(users)
 
 
-async def is_banned_user(user_id: int) -> bool:
+async def is_muted_user(user_id: int) -> bool:
     user = await blockeddb.find_one({"user_id": user_id})
     if not user:
         return False
     return True
 
 
-async def add_banned_user(user_id: int):
-    is_gbanned = await is_banned_user(user_id)
-    if is_gbanned:
+async def add_muted_user(user_id: int):
+    is_gbanned = await is_muted_user(user_id)
+    if is_gmuted:
         return
     return await blockeddb.insert_one({"user_id": user_id})
 
 
 async def remove_banned_user(user_id: int):
-    is_gbanned = await is_banned_user(user_id)
-    if not is_gbanned:
+    is_gmuted = await is_muted_user(user_id)
+    if not is_gmuted:
         return
     return await blockeddb.delete_one({"user_id": user_id})
 
